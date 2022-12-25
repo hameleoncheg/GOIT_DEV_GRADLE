76dc0ce8f036dd5a109f5cea9555123631fcb9ec
hameleoncheg <hameleoncheg@gmail.com>
2022-12-25 18:54:14 +0200

    Initial commit

---
+++ src/main/java/project/MyApp.java
@@ -0,0 +1,13 @@
+package project;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class MyApp {
+    public static void main(String[] args) {
+        Person person = new Person("Oleksii","Shevchuk");
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(person);
+        System.out.println(json);
+    }
+}
